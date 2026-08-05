cd66179f9e3c87c8a61042662a926901d1e6428f
mwibutsa <mwibutsa.dev@gmail.com>
2026-08-05 09:20:04 +0200

    test: nats streaming server pub/sub

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,19 @@
+console.clear();
+import nats from "node-nats-streaming";
+import type { Message } from "node-nats-streaming";
+
+const stan = nats.connect("ticketing", "123", {
+  url: "http://localhost:4222",
+});
+
+stan.on("connect", () => {
+  console.log("Listener connected to NATS");
+  const subscription = stan.subscribe("ticket:created");
+
+  subscription.on("message", (msg: Message) => {
+    const data = msg.getData();
+    if (typeof data === "string") {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+  });
+});
